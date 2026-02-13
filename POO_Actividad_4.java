50f2cfbe397e71fa2ac7b86db394c33d1843f19a
remixan07 <remixan07@gmail.com>
2026-02-13 06:21:31 -0600

    Actividad 4 - Versión 1 - 11/02/26 - 15:43

---
+++ POO_Actividad_4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class POO_Actividad_4 {
+
+    public static void main(String[] args) {
+
+        Subject s1 = new Subject();
+        Course c = new Course();
+        Student st = new Student();
+        Professor p = new Professor();
+
+        System.out.println("System running...");
+    }
+}
+
+class Subject {
+    public String name;
+}
+
+class Course {
+    public String name;
+}
+
+class Student {
+    public String name;
+}
+
+class Professor {
+    public String name;
+}
